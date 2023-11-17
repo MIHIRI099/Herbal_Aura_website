57f023be49e97cf9a0fc1afd1345a3e9125be629
WIJETHUNGA-174 <2019e174@eng.jfn.ac.lk>
2023-11-17 12:39:30 +0530

    hedder changed

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -1,8 +1,8 @@
 import { Outlet } from "react-router-dom";
-import Header from "./Header";
+import Header from "./components/Header.jsx";
 
 export default function Layout(){
-return (
+return (    
     <div className="p-4 flex flex-col min-h-screen">
         <Header/>
         <Outlet/>
