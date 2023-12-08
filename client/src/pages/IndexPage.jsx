a344701bc706f0c7d5e2442a024e3f31982cfea5
WIJETHUNGA-174 <2019e174@eng.jfn.ac.lk>
2023-12-08 21:51:46 +0530

    product

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -32,6 +32,30 @@ const allProducts = [
   {
     id: 7, image: '/images/img7.png',name: 'Body wash', price: 49.99,category:'Skin care products',
   },
+  {
+    id: 8, image: '/images/img1.png',name: 'Herbal oil product', price: 29.99,  category: 'Hair oil products',
+  },
+  {
+    id: 9, image: '/images/img2.png',name: 'Nutrition product', price: 39.99,  category: 'Wellness products',
+  },
+  {
+    id: 10, image: '/images/img3.png',name: 'Hare care product', price: 49.99,  category: 'Hair care products',
+  },
+  {
+    id: 11, image: '/images/img4.png',name: 'Immunity Boosters', price: 59.99,  category: 'Wellness products',
+  },
+  {
+    id: 12, image: '/images/img5.png',name: 'Face wash', price: 39.99,  category: 'Skin care products',
+  },
+  {
+    id: 13, image: '/images/img6.png',name: 'Face cream', price: 89.99, category:'Skin care products',
+  },
+  {
+    id: 14, image: '/images/img7.png',name: 'Body wash', price: 49.99,category:'Skin care products',
+  },
+  {
+    id: 15, image: '/images/img7.png',name: 'Body wash', price: 49.99,category:'Skin care products',
+  },
   // ... your product data ...
 ];
 
