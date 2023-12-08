37f19508eca3fbfd5b335e729a26b37630a43102
Mihiri <110478627+MIHIRI099@users.noreply.github.com>
2023-12-08 22:34:54 +0530

    ui_update

--- client/src/pages/CheckOutpage.jsx
+++ client/src/pages/CheckOutpage.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import NavigationBar from '../components/NavigationBar';
-import CartTile from '../components/CartTile'; // Import your CartTile component
+// Import your CartTile component
 
 const CheckoutPage = () => {
   const [name, setName] = useState('John Doe'); // Replace with data from your database
@@ -32,7 +32,7 @@ const CheckoutPage = () => {
         </Link>
       </div>
 
-      <div className="flex">
+      <div className="flex justify-center">
         {/* Left Side */}
         <div className="w-2/3 p-4">
           <h1 className="text-xl font-semibold mb-4">Checkout</h1>
@@ -46,7 +46,7 @@ const CheckoutPage = () => {
                 className="border p-2 w-full"
                 disabled
               />
-              <button onClick={handleEditClick} className="ml-2 bg-blue-500 text-white px-4 py-2">
+              <button onClick={handleEditClick} className="ml-2 bg-green-500 text-white px-4 py-2">
                 Edit
               </button>
             </div>
@@ -61,7 +61,7 @@ const CheckoutPage = () => {
                 className="border p-2 w-full"
                 disabled
               />
-              <button onClick={handleEditClick} className="ml-2 bg-blue-500 text-white px-4 py-2">
+              <button onClick={handleEditClick} className="ml-2 bg-green-500 text-white px-4 py-2">
                 Edit
               </button>
             </div>
@@ -89,7 +89,7 @@ const CheckoutPage = () => {
               </label>
             </div>
           </div>
-          <button onClick={handleContinueClick} className="bg-blue-500 text-white px-4 py-2">
+          <button onClick={handleContinueClick} className="bg-green-500 text-white px-4 py-2">
             Continue
           </button>
           <Link to="/cart" className="bg-gray-300 text-gray-700 px-4 py-2 ml-4">
@@ -98,20 +98,7 @@ const CheckoutPage = () => {
         </div>
 
         {/* Right Side - Cart Summary */}
-        <div className="w-1/3 p-4">
-          <h2 className="text-xl font-semibold mb-4">Cart Summary</h2>
-          {/* Map through cart items and display CartTile component */}
-          {/* Replace the dummy data below with your actual cart items */}
-          <CartTile
-            image="url_to_image"
-            name="Product Name"
-            description="Product Description"
-            price={29.99}
-            quantity={2}
-            onRemove={() => console.log('Remove item')}
-          />
-          {/* Add more CartTile components for other cart items */}
-        </div>
+        
       </div>
     </div>
   );
